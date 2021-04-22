53d0e0285bf4f6f9f2ca22b79d426b3348c221ac
JorgeVictoria <jvandreu@hotmail.com>
2021-04-23 01:32:32 +0200

    test
    twat

--- 011 - Java/Tema 02/temario/src/PrimerPrograma.java
+++ 011 - Java/Tema 02/temario/src/PrimerPrograma.java
@@ -2,8 +2,7 @@ public class PrimerPrograma { //inicio de la clase
     public static void main(String[] args) throws Exception { //inicio del main
 
         //imprimimos mensaje en pantalla
-        System.out.println("Hello mundo!");
-        System.out.println("yeepa");
+        System.out.println("Hola mundo!");
         
     } //fin del main
 
