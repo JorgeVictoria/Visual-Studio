847625b1a06a4869ea08a9df487ac79994e69703
JorgeVictoria <jvandreu@hotmail.com>
2021-04-23 01:18:38 +0200

    MacBook 23-04-2021
    inicio de curso

---
+++ 011 - Java/Tema 02/temario/src/PrimerPrograma.java
@@ -0,0 +1,9 @@
+public class PrimerPrograma { //inicio de la clase
+    public static void main(String[] args) throws Exception { //inicio del main
+
+        //imprimimos mensaje en pantalla
+        System.out.println("Hola mundo!");
+        
+    } //fin del main
+
+} //fin de la clase
