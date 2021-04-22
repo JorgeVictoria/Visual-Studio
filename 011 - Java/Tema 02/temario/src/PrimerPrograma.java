72d14514be39b989be41c78c44dfdb71bb1579f4
JorgeVictoria <jvandreu@hotmail.com>
2021-04-23 01:28:44 +0200

    test macbook

--- 011 - Java/Tema 02/temario/src/PrimerPrograma.java
+++ 011 - Java/Tema 02/temario/src/PrimerPrograma.java
@@ -2,7 +2,8 @@ public class PrimerPrograma { //inicio de la clase
     public static void main(String[] args) throws Exception { //inicio del main
 
         //imprimimos mensaje en pantalla
-        System.out.println("Hola mundo!");
+        System.out.println("Hello mundo!");
+        System.out.println("yeepa");
         
     } //fin del main
 
