2f5c02d679776cc21e285a5828821ead18f98c9b
Jorge Victoria Andreu <JorgeVictoria@users.noreply.github.com>
2021-05-05 17:44:20 +0200

    borrado

--- 011 - Java/Tema 02/temario/src/PrimerPrograma.java
+++
@@ -1,9 +0,0 @@
-public class PrimerPrograma { //inicio de la clase
-    public static void main(String[] args) throws Exception { //inicio del main
-
-        //imprimimos mensaje en pantalla
-        System.out.println("Hola mundo!");
-        
-    } //fin del main
-
-} //fin de la clase
--- 011 - Java/Tema 07/temario/src/U07Ejemplo01.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class U07Ejemplo01 {
-    
-    public static void main(String[] args) {
-        //primer argumento en la ventana padre, null el sistema
-        JOptionPane.showMessageDialog(null, "Hola mundo!");
-    }
-}
